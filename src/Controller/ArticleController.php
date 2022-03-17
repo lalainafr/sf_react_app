655d951606411cd8a863f385ff71aceb456080ed
Lalaina RAJAONAHSOA <r.lalainafr@gmail.com>
2022-03-17 17:47:08 +0100

    CRUD - Delete article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +20,18 @@ class ArticleController extends AbstractController
         return $this->render('/article/index.html.twig', [
             "articles" => $articles,
         ]);
+    }
+
+    /**
+     * @Route("/admin/article/{id}/delete", name="app_article_list")
+     */
+    public function deleteArticle($id, ArticleRepository $articleRepository, EntityManagerInterface $em)
+    {
+        $article = $articleRepository->find($id);
+
+        $em->remove($article);
+        $em->flush();
+
+        return $this->redirectToRoute('app_article_list');
     }
 }
